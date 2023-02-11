212b97d5795488d4117fce1e4ceab836e3523bfc
karthick-192211057 <123526175+karthick-192211057@users.noreply.github.com>
2023-02-11 18:48:36 +0530

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,8 @@
+str1=input("enter a string: ")
+str2=input("enter a string:")
+string1=str1.upper()
+string2=str2.upper()
+if (sorted (str1)== sorted (str2)):
+    print("it is a anagram")
+else :
+    print("it is not a anagram")
