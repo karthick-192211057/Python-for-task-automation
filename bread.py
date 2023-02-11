dbfd7e4e8603bfac72b7e365a392c048abb29f9d
karthick-192211057 <123526175+karthick-192211057@users.noreply.github.com>
2023-02-11 18:45:44 +0530

    Add files via upload

---
+++ bread.py
@@ -0,0 +1,8 @@
+new =int(input("enter no of new breads: "))
+old =int(input("enter no of old breads: "))
+a=new*185
+b=old*74
+c=a+b
+print("rate of new bread = ",a)
+print("Rate of old bread = ",b)
+print("total rate = ",c)
